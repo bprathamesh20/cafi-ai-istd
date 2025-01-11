f8dac948f767c6f6f9c3604bbe53a7187d81f034
bprathamesh20 <bprathamesh2003@gmail.com>
2025-01-11 11:52:08 +0530

    fix cache

--- app/api/connection-details/route.ts
+++ app/api/connection-details/route.ts
@@ -5,9 +5,13 @@ import {
 } from "livekit-server-sdk";
 import { NextResponse } from "next/server";
 
+// NOTE: you are expected to define the following environment variables in `.env.local`:
 const API_KEY = process.env.LIVEKIT_API_KEY;
 const API_SECRET = process.env.LIVEKIT_API_SECRET;
 const LIVEKIT_URL = process.env.LIVEKIT_URL;
+
+// don't cache the results
+export const revalidate = 0;
 
 export type ConnectionDetails = {
   serverUrl: string;
@@ -18,33 +22,38 @@ export type ConnectionDetails = {
 
 export async function GET() {
   try {
-    // Generate participant token
-    const participantIdentity = `voice_assistant_user_${Math.round(
-      Math.random() * 10_000
-    )}`;
-    const participantToken = await createParticipantToken(
-      {
-        identity: participantIdentity,
-      },
-      "roomName"
-    );
-
     if (LIVEKIT_URL === undefined) {
       throw new Error("LIVEKIT_URL is not defined");
     }
+    if (API_KEY === undefined) {
+      throw new Error("LIVEKIT_API_KEY is not defined");
+    }
+    if (API_SECRET === undefined) {
+      throw new Error("LIVEKIT_API_SECRET is not defined");
+    }
+
+    // Generate participant token
+    const participantIdentity = `voice_assistant_user_${Math.floor(Math.random() * 10_000)}`;
+    const roomName = `voice_assistant_room_${Math.floor(Math.random() * 10_000)}`;
+    const participantToken = await createParticipantToken(
+      { identity: participantIdentity },
+      roomName,
+    );
 
     // Return connection details
     const data: ConnectionDetails = {
       serverUrl: LIVEKIT_URL,
-      roomName: "voice_assistant_room",
+      roomName,
       participantToken: participantToken,
       participantName: participantIdentity,
     };
-    return NextResponse.json(data);
+    const headers = new Headers({
+      "Cache-Control": "no-store",
+    });
+    return NextResponse.json(data, { headers });
   } catch (error) {
     if (error instanceof Error) {
       console.error(error);
-
       return new NextResponse(error.message, { status: 500 });
     }
   }
@@ -54,8 +63,10 @@ function createParticipantToken(
   userInfo: AccessTokenOptions,
   roomName: string
 ) {
-  const at = new AccessToken(API_KEY, API_SECRET, userInfo);
-  at.ttl = "5m";
+  const at = new AccessToken(API_KEY, API_SECRET, {
+    ...userInfo,
+    ttl: "15m",
+  });
   const grant: VideoGrant = {
     room: roomName,
     roomJoin: true,
